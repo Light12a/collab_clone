6fa72cd1efd7843d2031440de915984338a93285
nhuutrong <nhuutrong@tma.com.vn>
2022-03-15 15:19:36 +0700

    Custom response code

--- utils/response.py
+++ utils/response.py
@@ -17,7 +17,7 @@ class ResponseMixin(object):
         self.finish()
 
     def write_response(self,
-                       status,
+                       result_code,
                        response_data=None,
                        code=None,
                        message=None,
@@ -29,11 +29,11 @@ class ResponseMixin(object):
             self.set_status(code)
         self.write(
             self.generate_response_data(
-                status, response_data, message, additional_data))
+                result_code, response_data, message, additional_data))
         self.finish()
 
     def generate_response_data(self,
-                               status,
+                               result_code,
                                response_data=None,
                                message=None,
                                additional_data=None):
@@ -48,40 +48,35 @@ class ResponseMixin(object):
                                 object with.
         """
         data = {
-            'status': status,
+            'resultCode': result_code,
         }
-        if additional_data is not None:
-            data.update(additional_data)
-        if response_data is not None:
-            if isinstance(response_data, list):
-                data['records'] = len(response_data)
-                data['data'] = response_data
-            else:
-                data['records'] = 1
-                data['data'] = [response_data]
-            self.response_length = data['records']
-
         if message is not None:
             data['message'] = message
 
+        if additional_data is not None:
+            data.update(additional_data)
+
+        if response_data is not None:
+            data['data'] = response_data
+
         return data
 
-    def success(self, response_data, additional_data=None):
-        status_code = HTTPStatus.OK.value
-        self.write_response('Success', response_data, status_code,
-                            additional_data=additional_data)
+    # def success(self,result_code, response_data, additional_data=None):
+    #     status_code = HTTPStatus.OK.value
+    #     self.write_response(result_code, response_data, status_code,
+    #                         additional_data=additional_data)
 
-    def deleted(self, response_data):
+    def deleted(self, result_code):
         status_code = HTTPStatus.OK.value
-        self.write_response('Success', None, status_code, 'Record(s) Deleted.')
+        self.write_response(result_code, None, status_code, 'Record(s) Deleted.')
 
-    def created(self, response_data, message='Record(s) Created.'):
+    def created(self, result_code, response_data, message='Record(s) Created.'):
         status_code = HTTPStatus.CREATED.value
-        self.write_response('Success', response_data, status_code, message)
+        self.write_response(result_code, response_data, status_code, message=message)
 
-    def updated(self, response_data, message='Record Updated.'):
+    def updated(self, result_code, response_data, message='Record Updated.'):
         status_code = HTTPStatus.OK.value
-        self.write_response('Success', response_data, status_code, message)
+        self.write_response(result_code, response_data, status_code, message=message)
 
     def no_content(self):
         status_code = HTTPStatus.NO_CONTENT.value
@@ -91,30 +86,30 @@ class ResponseMixin(object):
         self.finish()
         raise Finish()
 
-    def not_found(self, message):
+    def not_found(self, result_code, message):
         status_code = HTTPStatus.NOT_FOUND.value
-        self.write_response('Failure', None, status_code, message)
+        self.write_response(result_code, None, status_code, message=message)
 
-    def not_allowed(self):
+    def not_allowed(self, result_code):
         status_code = HTTPStatus.METHOD_NOT_ALLOWED.value
         message = 'Not Allowed'
-        self.write_response('Failure', None, status_code, )
+        self.write_response(result_code, None, status_code, message=message)
 
-    def fail(self, response_data=None, message=None, code=400):
-        status_code = code
-        self.write_response('Failure', response_data, status_code, message)
+    # def fail(self, response_data=None, message=None, code=400):
+    #     status_code = code
+    #     self.write_response('Failure', response_data, status_code, message)
 
-    def conflict(self, message='Record Already Exists.'):
+    def conflict(self, result_code, message='Record Already Exists.'):
         status_code = HTTPStatus.CONFLICT.value
-        self.write_response('Failure', code=status_code, message=message)
+        self.write_response(result_code, code=status_code, message=message)
 
-    def unprocessable_entity(self, message):
+    def unprocessable_entity(self, result_code, message):
         status_code = HTTPStatus.UNPROCESSABLE_ENTITY.value
-        self.write_response('Failure', code=status_code, message=message)
+        self.write_response(result_code, code=status_code, message=message)
 
-    def too_many_requests(self, message='Too many requests'):
+    def too_many_requests(self, result_code, message='Too many requests'):
         status_code = HTTPStatus.TOO_MANY_REQUESTS.value
-        self.write_response('Failure', code=status_code, message=message)
+        self.write_response(result_code, code=status_code, message=message)
 
     def forbidden(self):
         """ Do not send any response data.
@@ -140,11 +135,13 @@ class ResponseMixin(object):
         """ The request failed during execution of the input.
         An exception was thrown or other non user input related issue.
         """
-        result = {'status': 'error', 'message': message}
+        result = {'status': 'error'}
         if response_data:
             result['data'] = response_data
         if code:
             result['code'] = code
+        if message:
+            result['message'] = message
         self.write(result)
         self.set_status(code)
         self.finish()
