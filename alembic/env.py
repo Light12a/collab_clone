f50b36416e7ab844e3f01c1b71d7f7a23aef387d
vinhmaii <vinhmai0505@gmail.com>
2022-03-16 17:45:27 +0700

    correspondin_memo

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from handlers.tenant_settings.models import Base as BaseTenant
+from handlers.users.models import Base as BaseUser
+from handlers.sound_settings.models import Base as BaseSound
+from handlers.corresponding_memo.models import Base as BaseCorresMemo
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [BaseTenant.metadata, BaseUser.metadata, BaseCorresMemo.metadata, BaseSound.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- handlers/corresponding_memo/handlers.py
+++ handlers/corresponding_memo/handlers.py
@@ -1,20 +1,77 @@
-from sqlalchemy import Column, Date, DateTime, ForeignKey, String, Table, Text, Time, text, inspect
-from sqlalchemy.dialects.mysql import BIGINT, INTEGER, LONGTEXT, TINYINT, VARCHAR
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.sqltypes import NullType
-from sqlalchemy.ext.declarative import declarative_base
-from ..tenant_settings.models import Tenant
-Base = declarative_base()
-metadata = Base.metadata
+import imp
+import datetime
+import uuid
+import json
+from ..base import BaseHandler
+from .models import SoundFiles, User, Token
+from http import HTTPStatus
+from utils.config import config
+from utils.response import ResponseMixin
+from tornado import gen
+from services.logging import logger as log
+
+
+log = log.get(__name__)
+
+class GetCorrespondenceMemoHandler(ResponseMixin, BaseHandler):
+    """
+    This class is created to build API for get correspondence memo.
+    Params of request is token.
+    """
+    @property
+    def db(self):
+        return self.application.session
+
+    def data_received(self, chunk=None):
+        if self.request.body:
+            return json.loads(bytes.decode(self.request.body))
+
+    @gen.coroutine
+    def post(self):
+        try:
+            data = self.data_received()
+            if 'token' in data:
+                check, token = self._check_token_exists(data["token"])
+                if check:
+                    pass
+            else: raise ValueError
+        except ValueError:
+            self.write_response("Error", code=HTTPStatus.BAD_REQUEST.value, message="Bad request")
+            err = "Token {} in wrong format"
+            log.debug(err.format(data['token']))
+
+    @gen.coroutine
+    def _check_token_exists(self, token):
+        """
+            Meaning: Checking the token's existence
+            Input: token
+            Output: False  or
+                    True and token
+        """
+        result = self.db.query(Token.token_id).filter(Token.token_id == token)
+        if result == None:
+            raise gen.Return(False)
+        else:
+            raise gen.Return(True, token)
+    
+    def _get_correspondence_memo(self):
+        results = self.db.query.all()
+        if results:
+             memo =[{
+                "id": result[0][0],
+                "text": result[0][2]
+                } for result in results]
+        else:
+            resp = {
+            "code": 404,
+            "errorMessage": "No data"
+            }
+            err = "Not found any correspondence memo"
+            log.error(err)
+            self.write_response("Failure",)
+       
+        self.write_response
 
 
 
 
-class CorrespondenceMemo(Base):
-    __tablename__ = 'correspondence_memo'
-
-    correspondence_memo_id = Column(INTEGER(11), primary_key=True)
-    tenant_id = Column(INTEGER(11), index=True)
-    correspondence_memo_tag = Column(INTEGER(11))
-    correspondence_memo_explaination = Column(String(45))
-    last_change_date = Column(DateTime)
