b70838fe77408001457ce2fb9c4b851b7542b30c
ntiencong <ntiencong@tma.com.vn>
2022-03-24 11:13:24 +0700

    Base for projects feature

--- application/urls.py
+++ application/urls.py
@@ -11,3 +11,4 @@ urlpatterns += handlers.authority.urls.urlpatterns
 urlpatterns += handlers.global_settings.urls.urlpatterns
 urlpatterns += handlers.groups.urls.urlpatterns
 urlpatterns += handlers.callflow_settings.urls.urlpatterns
+urlpatterns += handlers.project_settings.urls.urlpatterns
