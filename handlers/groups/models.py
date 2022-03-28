260d007ef51e1f63641a3ba85e4235691c4f788e
nhuutrong <nhuutrong@tma.com.vn>
2022-03-28 18:48:48 +0700

    Update models for groups

--- handlers/groups/models.py
+++ handlers/groups/models.py
@@ -2,17 +2,17 @@ from sqlalchemy import Column, Date, DateTime, ForeignKey, String, Table, Text, 
 from sqlalchemy.dialects.mysql import BIGINT, INTEGER, LONGTEXT, TINYINT, VARCHAR
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import NullType
-from services.database.mysqldb import Base
 from handlers.tenant_settings.models import Tenant
 from handlers.authority.models import Authority
+from services.database.mysqldb import Base
 
 class Group(Base):
     __tablename__ = 'groups'
 
-    id = Column(BIGINT(20), nullable=False, unique=True)
-    tenant_id = Column(ForeignKey(Tenant.tenant_id), primary_key=True, nullable=False)
-    group_id = Column(String(256), primary_key=True, nullable=False)
-    group_name = Column(String(256))
+    id = Column(BIGINT(20), primary_key=True, nullable=False, unique=True)
+    tenant_id = Column(ForeignKey(Tenant.tenant_id), primary_key=True, nullable=False, index=True)
+    group_id = Column(VARCHAR(256), primary_key=True, nullable=False, index=True)
+    group_name = Column(VARCHAR(256))
     auth_id = Column(BIGINT(20), ForeignKey(Authority.auth_id), nullable=False)
     autoin_time = Column(INTEGER(11))
     insert_date = Column(DateTime, nullable=False, server_default=text("current_timestamp()"))
